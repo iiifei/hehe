99bd873ac78f3aac036ca9808b0f7d97bf331f46
tianyifei <tianyifei@longshine.com>
2021-08-03 15:44:22 +0800

    feat: test

---
+++ gaga/src/com/aa/controller/MyTest.java
@@ -0,0 +1,9 @@
+package com.aa.controller;
+
+public class MyTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("a - " + i);
+        }
+    }
+}
